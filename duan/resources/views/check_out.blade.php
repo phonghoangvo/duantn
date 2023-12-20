b8f2e4530a5ad8dbffab06943f3f2302a8a27fe3
Khavd46 <149936931+Khavd46@users.noreply.github.com>
2023-12-20 12:29:30 +0700

    view check out

--- duan/resources/views/check_out.blade.php
+++ duan/resources/views/check_out.blade.php
@@ -1,4 +1,21 @@
 @extends('layout')
 @section('noidung')
-    
+<div class="container pt-5 pb-5">
+    <div class='row'>
+        <h1 class="pt-5 pb-5">
+            Đặt hàng thành công 
+            <i class="bi bi-check-circle-fill" style="color: green"></i>
+        </h1>
+        <div class='col-md-12 pt-5 pb-5'>
+            <div class="card">
+                <div class="card-header pt-5 pb-5">
+                    Chúng tôi đã nhận được đơn hàng và sẽ vận chuyển đến bạn sớm nhất
+                </div>
+                <div class="card-body">
+                    <a href="{{ url('/') }}" class="btn btn-info text-dark"> <i class="fa fa-arrow-left"></i>Tiếp tục mua sắm</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
