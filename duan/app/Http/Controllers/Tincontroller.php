f0bb15eb1bdee98ac3217b909096c89f264096eb
KhoaPhan1010 <phandangkhoa10102003@gmail.com>
2024-01-16 09:05:59 +0700

    main

--- duan/app/Http/Controllers/Tincontroller.php
+++ duan/app/Http/Controllers/Tincontroller.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\DB;
 use App\Models\Tintuc;
@@ -12,6 +13,7 @@ use App\Models\Category;
 use App\Models\pro_cate;
 use App\Models\nhaxuatban;
 use App\Models\tacgia;
+
 Paginator::useBootstrap();
 
 use Illuminate\Http\Request;
@@ -34,17 +36,17 @@ class Tincontroller extends Controller
             ->take(6)
             ->get();
 
-        $danhchoban= DB::table('product')
-        ->where('hidden', 1)
-        ->orderBy(DB::raw('CAST(luotxem AS SIGNED)'), 'desc')
-        ->take(6)
-        ->get();
+        $danhchoban = DB::table('product')
+            ->where('hidden', 1)
+            ->orderBy(DB::raw('CAST(luotxem AS SIGNED)'), 'desc')
+            ->take(6)
+            ->get();
 
-        $yeuthich= DB::table('product')
-        ->where('hidden', 1)
-        ->orderBy(DB::raw('CAST(yeuthich AS SIGNED)'), 'desc')
-        ->take(6)
-        ->get();
+        $yeuthich = DB::table('product')
+            ->where('hidden', 1)
+            ->orderBy(DB::raw('CAST(yeuthich AS SIGNED)'), 'desc')
+            ->take(6)
+            ->get();
 
         $danhmucsach = DB::table('category')
             ->select('id', 'name')
@@ -54,7 +56,7 @@ class Tincontroller extends Controller
 
 
         $nhaxuatban = DB::table('nhaxuatban')
-            ->select('name','img')
+            ->select('name', 'img')
             ->get();
 
         return view('index', [
@@ -68,212 +70,223 @@ class Tincontroller extends Controller
     }
 
     public function cuahang($id = null)
-{
-    $perpage = 24;
-    $danhmucsach = null;
+    {
+        $perpage = 24;
+        $danhmucsach = null;
 
-    // Kiểm tra xem có $id được truyền vào hay không
-    if ($id !== null) {
-        // Nếu có $id được truyền vào, thực hiện tìm kiếm theo thể loại
-        $products = Cuahang::where('id', $id)->paginate($perpage)->withQueryString();
-    } else {
-        // Ngược lại, hiển thị tất cả sản phẩm
-        $products = Cuahang::paginate($perpage)->withQueryString();
-    }
-
-    // Lấy danh mục sách từ tất cả sản phẩm (lấy một lần, không cần lặp)
-    $idCategories = $products->pluck('id')->unique()->toArray();
-
-//     // Lọc và sắp xếp theo
-    $sort_by = isset($_GET['sort_by']) ? $_GET['sort_by'] : 'none';
-
-    switch ($sort_by) {
-        case 'giagiamdan':
-            $products = Cuahang::with('category')->whereIn('id', $idCategories)->orderBy('price', 'DESC')->paginate($perpage)->appends(request()->query());
-            break;
-        case 'giatangdan':
-            $products = Cuahang::with('category')->whereIn('id', $idCategories)->orderBy('price', 'ASC')->paginate($perpage)->appends(request()->query());
-            break;
-        case 'tuadenz':
-            $products = Cuahang::with('category')->whereIn('id', $idCategories)->orderBy('name', 'DESC')->paginate($perpage)->appends(request()->query());
-            break;
-        case 'tuzdena':
-            $products = Cuahang::with('category')->whereIn('id', $idCategories)->orderBy('name', 'ASC')->paginate($perpage)->appends(request()->query());
-            break;
-        default:
-            // Mặc định sắp xếp theo id giảm dần
-            $products = Cuahang::with('category')->whereIn('id', $idCategories)->orderBy('id', 'DESC')->paginate($perpage)->appends(request()->query());
-            break;
-    }
-
-    // Trả về view 'cuahang.blade.php' với dữ liệu sản phẩm
-    return view('cuahang', ['products' => $products, 'danhmucsach' => $danhmucsach]);
-}
-
-    
-public function timkiem(Request $request)
-{
-    $searchTerm = $request->input('timkiem');
-    $page = 24;
-
-    // Thực hiện tìm kiếm dựa trên $searchTerm
-    $productsQuery = Cuahang::with('category')
-        ->where('name', 'like', '%' . $searchTerm . '%');
-
-    // Thực hiện tìm kiếm theo tên danh mục
-    $productsQuery->orWhereHas('category', function ($q) use ($searchTerm) {
-        $q->where('name', 'like', '%' . $searchTerm . '%');
-    });
-
-    // Lọc theo
-    if ($sortBy = $request->input('sort_by')) {
-        $validSortOptions = ['giagiamdan', 'giatangdan', 'tuadenz', 'tuzdena'];
-        if (in_array($sortBy, $validSortOptions)) {
-            $direction = $sortBy === 'giatangdan' ? 'ASC' : 'DESC';
-            $productsQuery->orderBy('name', $direction)->orderBy('price', $direction);
+        // Kiểm tra xem có $id được truyền vào hay không
+        if ($id !== null) {
+            // Nếu có $id được truyền vào, thực hiện tìm kiếm theo thể loại
+            $products = Cuahang::where('id', $id)->paginate($perpage)->withQueryString();
+        } else {
+            // Ngược lại, hiển thị tất cả sản phẩm
+            $products = Cuahang::paginate($perpage)->withQueryString();
         }
+
+        // Lấy danh mục sách từ tất cả sản phẩm (lấy một lần, không cần lặp)
+        $idCategories = $products->pluck('id')->unique()->toArray();
+
+        //     // Lọc và sắp xếp theo
+        $sort_by = isset($_GET['sort_by']) ? $_GET['sort_by'] : 'none';
+
+        switch ($sort_by) {
+            case 'giagiamdan':
+                $products = Cuahang::with('category')->whereIn('id', $idCategories)->orderBy('price', 'DESC')->paginate($perpage)->appends(request()->query());
+                break;
+            case 'giatangdan':
+                $products = Cuahang::with('category')->whereIn('id', $idCategories)->orderBy('price', 'ASC')->paginate($perpage)->appends(request()->query());
+                break;
+            case 'tuadenz':
+                $products = Cuahang::with('category')->whereIn('id', $idCategories)->orderBy('name', 'DESC')->paginate($perpage)->appends(request()->query());
+                break;
+            case 'tuzdena':
+                $products = Cuahang::with('category')->whereIn('id', $idCategories)->orderBy('name', 'ASC')->paginate($perpage)->appends(request()->query());
+                break;
+            default:
+                // Mặc định sắp xếp theo id giảm dần
+                $products = Cuahang::with('category')->whereIn('id', $idCategories)->orderBy('id', 'DESC')->paginate($perpage)->appends(request()->query());
+                break;
+        }
+
+        // Trả về view 'cuahang.blade.php' với dữ liệu sản phẩm
+        return view('cuahang', ['products' => $products, 'danhmucsach' => $danhmucsach]);
     }
 
-    $products = $productsQuery->paginate($page)->appends(request()->query());
 
-    return view('cuahang', ['products' => $products]);
-}
+    public function timkiem(Request $request)
+    {
+        $searchTerm = $request->input('timkiem');
+        $page = 24;
+
+        // Thực hiện tìm kiếm dựa trên $searchTerm
+        $productsQuery = Cuahang::with('category')
+            ->where('name', 'like', '%' . $searchTerm . '%');
+
+        // Thực hiện tìm kiếm theo tên danh mục
+        $productsQuery->orWhereHas('category', function ($q) use ($searchTerm) {
+            $q->where('name', 'like', '%' . $searchTerm . '%');
+        });
+
+        // Lọc theo
+        if ($sortBy = $request->input('sort_by')) {
+            $validSortOptions = ['giagiamdan', 'giatangdan', 'tuadenz', 'tuzdena'];
+            if (in_array($sortBy, $validSortOptions)) {
+                $direction = $sortBy === 'giatangdan' ? 'ASC' : 'DESC';
+                $productsQuery->orderBy('name', $direction)->orderBy('price', $direction);
+            }
+        }
+
+        $products = $productsQuery->paginate($page)->appends(request()->query());
+
+        return view('cuahang', ['products' => $products]);
+    }
 
     //tacgia
     // 
-      
+
     //chitietsanpham
-    public function chitiet($id){ 
+    public function chitiet($id)
+    {
         $hot = DB::table('product')
             ->where('hot', 1)
             ->where('hidden', 1)
             ->orderBy('ngayDang', 'desc')
             ->limit(7)
             ->get();
-        $products = cuahang::where('id','=',$id)->get();
-        $comment = Comment::where('idProduct',$products[0]->id)->orderBy('id','DESC')->get();
-        $tg = Product::where('id','=',$id)->get();
+        $products = cuahang::where('id', '=', $id)->get();
+        $comment = Comment::where('idProduct', $products[0]->id)->orderBy('id', 'DESC')->get();
+        $tg = Product::where('id', '=', $id)->get();
         foreach ($products as $key => $value) {
             $idCategory = $value->idCategory;
         }
         $yeuthich = Product::where('id', '=', $id)->get();
         $sanphamlienquan = DB::table('pro_cate')
-            ->where('idCategory',$idCategory)
+            ->where('idCategory', $idCategory)
             ->limit(6)
             // ->join('product', 'pro_cate.idProduct', '=', 'product.id')
             ->get();
 
-        return view('chitiet',compact('products','hot','comment','yeuthich','tg'))
-        ->with('sanphamlienquan',$sanphamlienquan);
+        return view('chitiet', compact('products', 'hot', 'comment', 'yeuthich', 'tg'))
+            ->with('sanphamlienquan', $sanphamlienquan);
     }
-    public function favorite($idProduct){
+    public function favorite($idProduct)
+    {
         $data = [
             'idProduct' =>  $idProduct,
             'idUser' => auth()->id()
         ];
-        $yeuthich = Yeuthich::where(['idProduct' => $idProduct,'idUser'=> auth()->id()])->first();
-        if($yeuthich){
-            
+        $yeuthich = Yeuthich::where(['idProduct' => $idProduct, 'idUser' => auth()->id()])->first();
+        if ($yeuthich) {
+
             $yeuthich->delete();
-            return redirect()->back()->with('ok','Bạn đã bỏ yêu thích sản phẩm'); 
-             
-        }else{
+            return redirect()->back()->with('ok', 'Bạn đã bỏ yêu thích sản phẩm');
+        } else {
             Yeuthich::create($data);
-            return redirect()->back()->with('ok','Bạn đã yêu thích sản phẩm');  
+            return redirect()->back()->with('ok', 'Bạn đã yêu thích sản phẩm');
         }
     }
-    public function post_comment($proId){
+    public function post_comment($proId)
+    {
         $data = request()->all('content');
         $data['idProduct'] = $proId;
         $data['idUser'] = auth()->id();
-        if(comment::create($data)){
-            return redirect()->back()->with('success','Bình luận đã được gửi');
+        if (comment::create($data)) {
+            return redirect()->back()->with('success', 'Bình luận đã được gửi');
         }
         return redirect()->back()->with('error', 'Bình luận chua được gửi');
     }
 
     //Tintuc
     public function news()
-        {
-            $news = Tintuc::where('hidden', 1)
-                        ->orderBy('created_at', 'desc')
-                        ->paginate(8);
+    {
+        $news = Tintuc::where('hidden', 1)
+            ->orderBy('created_at', 'desc')
+            ->paginate(8);
 
-            return view('news', ['news' => $news]);
-        }
-
-    function chitietnew($id){
-        $chitietnew = Tintuc::find($id);
-        return view('chitietnew',['chitietnew'=>$chitietnew]);
+        return view('news', ['news' => $news]);
     }
-    public function listtintuc() {
+
+    function chitietnew($id)
+    {
+        $chitietnew = Tintuc::find($id);
+        return view('chitietnew', ['chitietnew' => $chitietnew]);
+    }
+    public function listtintuc()
+    {
         $listtintuc = Tintuc::orderBy('created_at', 'desc')
-                            ->paginate(5);
-    
+            ->paginate(5);
+
         return view('admin.listtintuc', compact('listtintuc'));
     }
-    
-    
 
-   
 
-    function themtin() {
+
+
+
+    function themtin()
+    {
         return view('admin.tin.themtin');
     }
 
-    function themtin_(Request $request) {
+    function themtin_(Request $request)
+    {
         $imgPath = $request->file('img')->store('img', 'public');
         $t = new Tintuc;
         $t->title = $request->input('title');
         $t->img = '/storage/' . $imgPath;
         $t->summary = $request->input('summary');
         $t->content = $request->input('content');
-        $t->hidden = $request->input('visibility',1);
+        $t->hidden = $request->input('visibility', 1);
         $t->save();
-    
-        return redirect()->route('listtintuc')-> with('success','Tin tức đã được thêm thành công');
-    }
-    
 
-    function xoa($id) {
+        return redirect()->route('listtintuc')->with('success', 'Tin tức đã được thêm thành công');
+    }
+
+
+    function xoa($id)
+    {
         $t = Tintuc::find($id);
         $t->delete();
         return redirect()->route('listtintuc');
     }
 
-    function suatin($id) {
+    function suatin($id)
+    {
         $tintuc = Tintuc::find($id);
         return view("admin.tin.suatin", compact('tintuc'));
-    }    
+    }
 
-    public function capnhat(Request $request, $id) {
+    public function capnhat(Request $request, $id)
+    {
         $tintuc = Tintuc::find($id);
-    
+
         // Kiểm tra xem người dùng đã tải lên ảnh mới hay chưa
         if ($request->hasFile('img')) {
             // Nếu có ảnh mới, lưu và cập nhật đường dẫn hình ảnh
             $imgPath = $request->file('img')->store('img', 'public');
             $tintuc->img = '/storage/' . $imgPath;
         }
-    
+
         // Cập nhật các trường khác
         $tintuc->title = $request->input('title');
         $tintuc->summary = $request->input('summary');
         $tintuc->content = $request->input('content');
         $tintuc->hidden = $request->input('visibility', 1);
-    
+
         // Lưu các thay đổi
         $tintuc->save();
-    
+
         // Chuyển hướng về trang danh sách tin tức
-        return redirect()->route('listtintuc')-> with('success','Tin tức đã được cập nhật thành công');
+        return redirect()->route('listtintuc')->with('success', 'Tin tức đã được cập nhật thành công');
     }
-    
-    function lienhe() {
+
+    function lienhe()
+    {
         return view('lienhe');
     }
 
-    function gioithieu() {
+    function gioithieu()
+    {
         return view('gioithieu');
     }
 }
--- duan/resources/views/cuahang.blade.php
+++ duan/resources/views/cuahang.blade.php
@@ -3,148 +3,151 @@
     Cửa hàng
 @endsection
 @section('noidung')
+    <div class="container px-0 pt-3 pb-3">
+        <a class="thanhphantrang text-decoration-none" href="">
+            Trang chủ
+        </a> /
+        <small>Cửa hàng</small>
 
-   
 
-<div class="container px-0 pt-3 pb-3">
-    <a class="thanhphantrang text-decoration-none" href="">
-        Trang chủ
-    </a> /
-    <small>Cửa hàng</small>
-    
-    
-</div>
-<div class="container pt-3 pb-3 px-0">
+    </div>
+    <div class="container pt-3 pb-3 px-0">
 
-    
-    <div class="row">
-        <section class="col-lg-3 ">
-            <div class=" boxloai mb-4 bg-white">
-                <h3 class="p-2">Sản phẩm theo loại</h3>
-                <div class="danhmuc">
+
+        <div class="row">
+            <section class="col-lg-3 ">
+                <div class=" boxloai mb-4 bg-white">
+                    <h3 class="p-2">Sản phẩm theo loại</h3>
+                    <div class="danhmuc">
+                        <?php
+                        $danhmuc = DB::table('category')
+                            ->select('id', 'name')
+                            ->orderByRaw('CAST(thutu AS SIGNED) ASC')
+                            ->where('hidden', '=', '1')
+                            ->get();
+                        ?>
+                        <ul>
+                            @foreach ($danhmuc as $dm)
+                                <li>
+                                    <a href="{{ route('cuahang', ['id' => $dm->id]) }}"><span>{{ $dm->name }}</span></a>
+                                </li>
+                            @endforeach
+
+
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="boxloai mb-4 bg-white">
+                    <h3 class="p-2">Tác giả</h3>
                     <?php
-                    $danhmuc = DB::table('category')->select('id','name')
-                    ->orderByRaw('CAST(thutu AS SIGNED) ASC')
-                    ->where('hidden','=','1')
-                    ->get();
+                    $tacgia = DB::table('tacgia')
+                        ->select('id', 'name')
+                        ->get();
                     ?>
-                    <ul>
-                        @foreach ($danhmuc as $dm)
-                        <li>
-                            <a href="{{ route('cuahang', ['id' => $dm->id]) }}"><span>{{$dm->name}}</span></a>
-                        </li>
+                    <div class="danhmuc">
+                        @foreach ($tacgia as $tg)
+                            <label><input type="checkbox" name="tacgia" value="tacgia{{ $tg->id }}">
+                                {{ $tg->name }}</label>
+                        @endforeach
+                    </div>
+                </div>
+
+                <div class="boxloai mb-4 bg-white">
+                    <h3 class="p-2">Nhà xuất bản</h3>
+                    <?php
+                    $nhaxuatban = DB::table('nhaxuatban')
+                        ->select('id', 'name')
+                        ->get();
+                    ?>
+                    <div class="danhmuc">
+                        @foreach ($nhaxuatban as $nhaxuatban)
+                            <label>
+                                <input type="checkbox" name="nhaxuatban" value="{{ $nhaxuatban->id }}">
+                                {{ $nhaxuatban->name }}
+                            </label>
                         @endforeach
 
 
-                    </ul>
-                </div>
-            </div>
-
-            <div class="boxloai mb-4 bg-white">
-                <h3 class="p-2">Tác giả</h3>
-                <?php
-                    $tacgia = DB::table('tacgia')
-                    ->select('id','name')
-                    ->get();
-                    ?>
-                <div class="danhmuc">
-                    @foreach ($tacgia as $tg)
-                        <label><input type="checkbox" name="tacgia" value="tacgia{{$tg->id}}"> {{$tg->name}}</label>
-
-                     @endforeach
-                </div>
-            </div>
-        
-            <div class="boxloai mb-4 bg-white">
-                <h3 class="p-2">Nhà xuất bản</h3>
-                <?php
-                $nhaxuatban = DB::table('nhaxuatban')
-                ->select('id','name')
-                ->get();
-                ?>
-                <div class="danhmuc">
-                    @foreach ($nhaxuatban as $nhaxuatban)
-                        <label>
-                            <input type="checkbox" name="nhaxuatban" value="{{ $nhaxuatban->id }}">
-                            {{ $nhaxuatban->name }}
-                        </label>
-                    @endforeach
-
-                
-                </div>
-            </div>
-            
-            <div class="boxloai mb-4 bg-white">
-                <h3 class="p-2">Lọc theo giá</h3>
-                <div class="locgia">
-                <p>
-                <label for="amount">Khoảng giá:</label>
-                <input type="text" id="amount" readonly style="border:0; color:#f6931f; font-weight:bold;">
-                </p>
-                <div id="slider-range"></div>
-                </div>
-               
-              
-
-            </div>
-        </section>
-        <section class="col-lg-9">
-            <div class="thanhchucnang bg-white">
-                <div class="row p-3" >
-                    <div class="col-sm-3 mt-2"> <label>Sắp xếp theo: </label></div>
-                    <div class="col-sm-5">
-                        <form action="">
-                            @csrf
-                            <select name="sort" id="sort" class="form-select thanhsapxep" aria-label="Default select example">
-                                <option value="{{Request::url()}}?sort_by=none">--Lọc Theo--</option>
-                                <option value="{{Request::url()}}?sort_by=giatangdan">--Giá tăng dần--</option>
-                                <option value="{{Request::url()}}?sort_by=giagiamdan">--Giá giảm dần--</option>
-                                <option value="{{Request::url()}}?sort_by=tuadenz">Lọc theo tên từ A đến Z</option>
-                                <option value="{{Request::url()}}?sort_by=tuzdena">Lọc theo tên từ Z đến A</option>
-                            </select>
-                        </form>
                     </div>
                 </div>
-                <hr>
-            </div>
-            <div class="box  bg-white">
-                <div class="row p-3 ">
-                    @foreach ($products as $sanpham)
-                    <div class="col-md-6 col-lg-4 col-xl-3 p-2 sanpham mb-2">
-                        <div class="boxsanpham  p-2"><a href="{{url('/chitiet/'.$sanpham->id)}}">
-                            <div class="img">
-                                <img src="{{$sanpham->img}}"
-                                    alt="{{$sanpham->name}}" width="200px" height="200px">
-                            </div>
-                            <div class="title">
-                                <h3>
-                                    <a href="{{url('/chitiet/'.$sanpham->id)}}">{{$sanpham->name}}</a>
-                                </h3>
-                            </div>
-                            <div class="row">
-                            <div class="col-8 gia">
-                                <span>{{number_format($sanpham->priceSale)}} đ</span><br>
-                                <p><del>{{number_format($sanpham->price)}} đ</del></p>
-                            </div>
-                            <div class="col-4">
-                                <a href="{{ route('add_to_cart', $sanpham->id) }}" class="btn btn float-end px-2" style="background-color:#f27024; padding: 5px 10px;border-radius: 50%; color:white;"><i class="bi bi-bag-plus-fill"></i></a>
-                            </div>
-                        </div>
-                            </a>
+
+                <div class="boxloai mb-4 bg-white">
+                    <h3 class="p-2">Lọc theo giá</h3>
+                    <div class="locgia">
+                        <p>
+                            <label for="amount">Khoảng giá:</label>
+                            <input type="text" id="amount" readonly
+                                style="border:0; color:#f6931f; font-weight:bold;">
+                        </p>
+                        <div id="slider-range"></div>
+                    </div>
+
+
+
+                </div>
+            </section>
+            <section class="col-lg-9">
+                <div class="thanhchucnang bg-white">
+                    <div class="row p-3">
+                        <div class="col-sm-3 mt-2"> <label>Sắp xếp theo: </label></div>
+                        <div class="col-sm-5">
+                            <form action="">
+                                @csrf
+                                <select name="sort" id="sort" class="form-select thanhsapxep"
+                                    aria-label="Default select example">
+                                    <option value="{{ Request::url() }}?sort_by=none">--Lọc Theo--</option>
+                                    <option value="{{ Request::url() }}?sort_by=giatangdan">--Giá tăng dần--</option>
+                                    <option value="{{ Request::url() }}?sort_by=giagiamdan">--Giá giảm dần--</option>
+                                    <option value="{{ Request::url() }}?sort_by=tuadenz">Lọc theo tên từ A đến Z</option>
+                                    <option value="{{ Request::url() }}?sort_by=tuzdena">Lọc theo tên từ Z đến A</option>
+                                </select>
+                            </form>
                         </div>
                     </div>
-                    @endforeach
-                    
-                    {{ $products->links() }}
-        </section>
+                    <hr>
+                </div>
+                <div class="box  bg-white">
+                    <div class="row p-3 ">
+                        @foreach ($products as $sanpham)
+                            <div class="col-md-6 col-lg-4 col-xl-3 p-2 sanpham mb-2">
+                                <div class="boxsanpham  p-2"><a href="{{ url('/chitiet/' . $sanpham->id) }}">
+                                        <div class="img">
+                                            <img src="{{ $sanpham->img }}" alt="{{ $sanpham->name }}" width="200px"
+                                                height="200px">
+                                        </div>
+                                        <div class="title">
+                                            <h3>
+                                                <a href="{{ url('/chitiet/' . $sanpham->id) }}">{{ $sanpham->name }}</a>
+                                            </h3>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-8 gia">
+                                                <span>{{ number_format($sanpham->priceSale) }}.000 đ</span><br>
+                                                <p><del>{{ number_format($sanpham->price) }}.000 đ</del></p>
+                                            </div>
+                                            <div class="col-4">
+                                                <a href="{{ route('add_to_cart', $sanpham->id) }}"
+                                                    class="btn btn float-end px-2"
+                                                    style="background-color:#f27024; padding: 5px 10px;border-radius: 50%; color:white;"><i
+                                                        class="bi bi-bag-plus-fill"></i></a>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        @endforeach
+
+                        {{ $products->links() }}
+            </section>
+        </div>
     </div>
-</div>
 
-{{-- <div class="pagination">
+    {{-- <div class="pagination">
     <a href="#">1</a>
     <a href="#">2</a>
     <a href="#">3</a>
     <a class="justify-content-center" href=""><i class="bi bi-chevron-right"></i></i></a>
 </div> --}}
-</div>
+    </div>
 @endsection
